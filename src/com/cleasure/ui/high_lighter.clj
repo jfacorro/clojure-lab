087b26fec873d6d07419cac046530ae314b2749d
Juan Facorro <juan.facorro@gmail.com>
2012-07-20 16:32:26 -0300

    Added upper panel for buttons.

--- src/com/cleasure/ui/high_lighter.clj
+++ src/com/cleasure/ui/high_lighter.clj
@@ -6,19 +6,20 @@
 (def style-constants {
 	:bold			StyleConstants/Bold, 
 	:foreground		StyleConstants/Foreground, 
-	:font-size		StyleConstants/FontSize})
+	:font-size		StyleConstants/FontSize,
+	:font-family	StyleConstants/FontFamily})
 
-(defn make-style [attrs]
+(defn defstyle [attrs]
 	(let [style (SimpleAttributeSet.)]
 		(doseq [[k v] attrs]
 			(. style addAttribute (k style-constants) v))
 		style))
 
-(def styles {	:keywords	(make-style {:bold true :foreground Color/blue})
-				:delimiters	(make-style {:bold true :foreground Color/red})
-				:default	(make-style {:bold false :foreground Color/black})})
+(def styles {	:keywords	(defstyle {:bold true :foreground Color/blue :font-family "Consolas" :font-size (int 14)})
+				:delimiters	(defstyle {:bold true :foreground Color/red :font-family "Consolas" :font-size (int 14)})
+				:default	(defstyle {:bold false :foreground Color/black :font-family "Consolas" :font-size (int 14)})})
 
-(def keywords #{"def" "defn" "fn" "ns"})
+(def keywords #{"def" "defn" "fn" "ns" "in-ns" "all-ns" "doseq"})
 (def delimiters #{"(" ")" "{" "}" "[" "]"})
 
 (def styles-map {	:keywords	keywords
@@ -28,7 +29,7 @@
 	"Finds all indexes where ptrn is matched in text and
 	return a list with the intervals in which de matches are
 	located."
-	(let [len (.length ptrn)]
+	(let [	len (.length ptrn)]
 		(loop [	start	0
 				idx 	(. text indexOf ptrn start)
 				idxs	[]]
@@ -37,14 +38,15 @@
 				(recur
 					idx 
 					(. text indexOf ptrn (+ idx len))
-					(conj idxs [idx (+ idx len)]))))))
+					(conj idxs idx))))))
 
 (defn high-light [txt-pane]
 	(let [	doc (. txt-pane getStyledDocument)
-			text (. txt-pane getText)]
-		(. doc setCharacterAttributes 0 (. text length) (:default styles) false)
+			text (. txt-pane getText)
+			stripped text] ;(.. text (replace "\n" ""))]
+		(. doc setCharacterAttributes 0 (. text length) (:default styles) true)
 		(doseq [[s kws] styles-map]
 			(doseq [kw kws]
-				(println " for " kw " found " (all-index-of text kw))
-				(doseq [[strt end] (all-index-of text kw)]
-					(. doc setCharacterAttributes strt (dec end) (s styles) false))))))
+				;(println " for " kw " found " (all-index-of stripped kw))
+				(doseq [idx (all-index-of stripped kw)]
+					(. doc setCharacterAttributes idx (.length kw) (s styles) true))))))
--- src/main.clj
+++ src/main.clj
@@ -1,8 +1,9 @@
 (ns com.cleajure.main
 	(:import
-		[javax.swing JFrame JScrollPane JTextPane JTextField JButton JFileChooser UIManager]
+		[javax.swing JFrame JPanel JScrollPane JTextPane JTextField JButton JFileChooser UIManager]
 		[javax.swing.text StyleContext DefaultStyledDocument]
-		[java.awt BorderLayout Font]
+		[javax.swing.event DocumentListener]
+		[java.awt BorderLayout FlowLayout Font]
 		[java.awt.event MouseAdapter KeyAdapter KeyEvent])
 	(:require 
 		[clojure.reflect :as r]
@@ -13,8 +14,6 @@
 (def ^:dynamic *app-name* "Cleajure")
 (def ^:dynamic *default-font* (Font. "Consolas" Font/PLAIN 14))
 
-
-
 ; Set native look & feel instead of Swings default
 (UIManager/setLookAndFeel (UIManager/getSystemLookAndFeelClassName))
 
@@ -22,74 +21,105 @@
 	"Checks if a string contains a substring"
 	(.contains (str s) ptrn))
 
-(defn list-methods 
+(defn list-methods
 	([c] (list-methods  c ""))
 	([c name]
-		(let 
-			[members	(:members (r/type-reflect c :ancestors true))
-			methods		(filter #(:return-type %) members)]
-				(filter
-					#(or (str-contains? % name) (empty name))
-					(sort (for [m methods] (:name m)))))))
+		(let [	members	(:members (r/type-reflect c :ancestors true))
+				methods		(filter #(:return-type %) members)]
+			(filter
+				#(or (str-contains? % name) (empty name))
+				(sort (for [m methods] (:name m)))))))
 
-(defn save-src [txt path]
-	(let [content (.getText txt)]
-		(with-open [wrtr (writer path)]
+(defn save-src [txt-code txt-path]
+	(let [content (.getText txt-code)]
+		(with-open [wrtr (writer (.getText txt-path))]
 			(.write wrtr content))))
 
-(defn load-src [txt-code txt-path]
-	(let
-		[dialog	(JFileChooser.)
-		result	(. dialog showOpenDialog (. txt-code getParent))
-		file	(. dialog getSelectedFile)
-		path	(if file (. file getPath) nil)]
+(defn remove-cr [str] 
+	"Removes carriage returns from the string."
+	(.replace str "\r" ""))
+
+(defn open-src [txt-code txt-path]
+	(let [	dialog	(JFileChooser.)
+			result	(. dialog showOpenDialog (. txt-code getParent))
+			file	(. dialog getSelectedFile)
+			path	(if file (. file getPath) nil)]
 		(when path
 			(. txt-path setText path)
-			(. txt-code setText (slurp path)))))
+			(. txt-code setText (remove-cr (slurp path))))))
 
 (defn eval-src [txt-code]
-	(load-string (.getText txt-code))
-	(print (.getText txt-code)))
+	(load-string (.getText txt-code)))
 
 (defn on-click [cmpt f]
 	(.addMouseListener cmpt 
 		(proxy [MouseAdapter] []
 			(mouseClicked [e] (f)))))
 
+(defn check-key [evt k m]
+	"Checks if the key and the modifier match with the event's values"
+	(and 	(or (= k (.getKeyCode evt)) (not k))
+			(or (= m (.getModifiers evt)) (not m))))
+
 (defn on-keypress
 	([cmpt f] (on-keypress cmpt f nil nil))
 	([cmpt f key mask]
 		(.addKeyListener cmpt
 			(proxy [KeyAdapter] []
-				(keyPressed [e]
-					(when (and 
-							(or (= key (.getKeyCode e)) (not key))
-							(or (= mask (.getModifiers e)) (not mask)))
-						(f)))))))
+				(keyPressed [e] (when (check-key e key mask) (f)))))))
+
+(defn on-keyrelease
+	([cmpt f] (on-keyrelease cmpt f nil nil))
+	([cmpt f key mask]
+		(.addKeyListener cmpt
+			(proxy [KeyAdapter] []
+				(keyReleased [e] (when (check-key e key mask) (f)))))))
+
+(defn on-text-changed [cmpt f]
+	(let [doc (. cmpt getStyledDocument)]
+		(.addDocumentListener doc
+			(proxy [DocumentListener] []
+				(insertUpdate [e] (f))
+				(removeUpdate [e] (f))))))
 
 (defn make-main [name]
 	(let 
-		[main		(JFrame. name)
-		txt-code	(JTextPane. (DefaultStyledDocument.))
-		txt-path	(JTextField.)
-		btn-save	(JButton. "Save")
-		btn-load	(JButton. "Load")
-		btn-eval	(JButton. "Eval")]
+		[	main		(JFrame. name)
+			txt-code		(JTextPane. (DefaultStyledDocument.))
+			txt-path		(JTextField.)
+			pnl-buttons	(JPanel.)
+			btn-save		(JButton. "Save")
+			btn-open		(JButton. "Open")
+			btn-eval		(JButton. "Eval")]
 		(.setFont txt-code *default-font*)
 		(.setEditable txt-path false)
-		(on-click btn-save #(save-src txt-code (.getText txt-path)))
-		(on-click btn-load #(load-src txt-code txt-path))
+
+		; Add buttons click handlers
+		(on-click btn-open #(open-src txt-code txt-path))
+		(on-click btn-save #(save-src txt-code txt-path))
 		(on-click btn-eval #(eval-src txt-code))
-		(on-keypress txt-code #(hl/high-light txt-code))
-		(on-keypress txt-code 
-			#(load-string (.getSelectedText txt-code)) 
-			KeyEvent/VK_ENTER 
+
+		; High-light text after key release.
+		(on-keyrelease txt-code #(hl/high-light txt-code))
+
+		; Open: CTRL + O
+		(on-keypress txt-code #(open-src txt-code txt-path) KeyEvent/VK_O KeyEvent/CTRL_MASK)
+		; Save: CTRL + S
+		(on-keypress txt-code #(save-src txt-code txt-path) KeyEvent/VK_S KeyEvent/CTRL_MASK)
+		; Eval: CTRL + Enter
+		(on-keypress txt-code #(println (load-string (.getSelectedText txt-code)))
+			KeyEvent/VK_ENTER
 			KeyEvent/CTRL_MASK)
+
+		; buttons panel
+		(doto pnl-buttons
+			(.setLayout (FlowLayout.))
+			(.add btn-save)
+			(.add btn-open)
+			(.add btn-eval))
 		(doto main
 			(.setSize 800 600)
-			(.add btn-save BorderLayout/NORTH)
-			(.add btn-load BorderLayout/WEST)
-			(.add btn-eval BorderLayout/EAST)
+			(.add pnl-buttons BorderLayout/NORTH)
 			(.add (JScrollPane. txt-code) BorderLayout/CENTER)
 			(.add txt-path BorderLayout/SOUTH)
 			(.show))))
