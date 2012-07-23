35ee5b4062d7f051b8f6eedac1a14a8d0014f2eb
Juan Facorro <juan.facorro@gmail.com>
2012-07-23 15:46:55 -0300

    Moved libs directory to project root.

--- src/debug.bat
+++ src/debug.bat
@@ -1,3 +1,3 @@
 cd \Juan\Dropbox\Facultad\Tesis\IDE\src
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n -cp libs/clojure-1.4.0.jar;. clojure.main -i com/cleasure/main.clj 
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n -cp ../libs/clojure-1.4.0.jar;. clojure.main -i com/cleasure/main.clj 
 pause
