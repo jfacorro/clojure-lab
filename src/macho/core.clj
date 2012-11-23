6bd89010fe1c69896d3bedb6fe10b34dae021b5e
Juan Facorro <juan.facorro@gmail.com>
2012-11-23 12:14:50 -0300

    Added automatic insertion of closing delimiters. Added input dialog.

--- src/macho/core.clj
+++ src/macho/core.clj
@@ -1,36 +1,38 @@
 (ns macho.core
   (:import [javax.swing JFrame JPanel JScrollPane JTextPane JTextArea 
             JTextField JButton JFileChooser UIManager JSplitPane JTree
-            SwingUtilities JTabbedPane JMenuBar JMenu JMenuItem KeyStroke]
+            SwingUtilities JTabbedPane JMenuBar JMenu JMenuItem KeyStroke
+            JOptionPane]
            [javax.swing.text StyleContext DefaultStyledDocument] 
            [javax.swing.undo UndoManager]
            [javax.swing.event DocumentListener]
            [java.io OutputStream PrintStream File OutputStreamWriter]
            [java.awt BorderLayout FlowLayout Font Color]
-           [java.awt.event MouseAdapter KeyAdapter KeyEvent ActionListener])
+           [java.awt.event MouseAdapter KeyAdapter KeyEvent ActionListener]
+           [javax.swing.text DefaultHighlighter$DefaultHighlightPainter])
   (:require [clojure.reflect :as r]
             [macho.ui.swing.highlighter :as hl :reload true]
             [macho.ui.swing.undo :as undo])
   (:use [clojure.java.io]))
-
+;;------------------------------
 (declare main tabs docs tree repl menu)
-
+;;------------------------------
 (def app-name "macho")
 (def new-doc-title "Untitled")
-
+;;------------------------------
 (def ^:dynamic *current-font* (Font. "Consolas" Font/PLAIN 14))
 (def default-dir (atom (.getCanonicalPath (File. "."))))
-
+;;------------------------------
 ;; Set the application look & feel instead of Swings default.
 (javax.swing.UIManager/setLookAndFeel "javax.swing.plaf.nimbus.NimbusLookAndFeel")
 ;; (UIManager/setLookAndFeel (UIManager/getSystemLookAndFeelClassName))
 ;; (UIManager/setLookAndFeel (UIManager/getCrossPlatformLookAndFeelClassName))
 ;; (UIManager/setLookAndFeel "com.sun.java.swing.plaf.motif.MotifLookAndFeel")
-
+;;------------------------------
 (defn str-contains? [s ptrn]
   "Checks if a string contains a substring"
   (.contains (str s) ptrn))
-
+;;------------------------------
 (defn list-methods
   ([c] (list-methods c ""))
   ([c name]
@@ -39,42 +41,42 @@
       (filter 
         #(or (str-contains? % name) (empty name))
         (sort (for [m methods] (:name m)))))))
-
+;;------------------------------
 (defn queue-ui-action [f]
   (SwingUtilities/invokeLater f))
-
+;;------------------------------
 (defn eval-code [code]
   (try
     (println (load-string code))
     (catch Exception e
          (println e)
 	(println (.getMessage e)))))
-
+;;------------------------------
 (defn on-click [cmpt f] 
   (.addActionListener cmpt
     (proxy [ActionListener] []
       (actionPerformed [e] (f)))))
-
+;;------------------------------
 (defn check-key [evt k m]
   "Checks if the key and the modifier match the event's values"
   (and 
     (or (nil? k) (= k (.getKeyCode evt)))
     (or (nil? m) (= m (.getModifiers evt))))) 
-
+;;------------------------------
 (defn on-keypress
   ([cmpt f] (on-keypress cmpt f nil nil))
   ([cmpt f key mask]
     (.addKeyListener cmpt
       (proxy [KeyAdapter] []
         (keyPressed [e] (when (check-key e key mask) (f e)))))))
-
+;;------------------------------
 (defn on-keyrelease
   ([cmpt f] (on-keyrelease cmpt f nil nil)) 
   ([cmpt f key mask]
     (.addKeyListener cmpt
       (proxy [KeyAdapter] []
         (keyReleased [e] (when (check-key e key mask) (f e)))))))
- 
+;;------------------------------
 (defn on-changed [cmpt f]
   (let [doc (.getStyledDocument cmpt)]
     (.addDocumentListener doc
@@ -82,14 +84,14 @@
         (changedUpdate [e] nil)
         (insertUpdate [e] (queue-ui-action f))
         (removeUpdate [e] (queue-ui-action f))))))
-
+;;------------------------------
 (defn current-txt [tabs]
   (let [idx (.getSelectedIndex tabs)
         scroll (.getComponentAt tabs idx)
         pnl (.. scroll getViewport getView) 
         txt (.getComponent pnl 0)]
   txt))
-
+;;------------------------------
 (defn current-path 
   "Finds the current working tab and shows a 
   file chooser window if it's a new file."
@@ -104,18 +106,33 @@
         (when path (.setTitleAt tabs idx path))
         path)
        path)))
-
+;;------------------------------
 (defn save-src [tabs]
   (let [txt-code (current-txt tabs)
         path (current-path tabs)
         content (.getText txt-code)]
     (when path
       (spit path content))))
-
+;;------------------------------
 (defn eval-src [tabs]
   (let [txt (current-txt tabs)]
     (eval-code (.getText txt))))
-
+;;------------------------------
+(defn find-src 
+  "Shows the dialog for searching the source
+  in the current tabs."
+  [tabs]
+  (let [txt  (current-txt tabs)
+        doc  (.getDocument txt)
+        hl   (.getHighlighter txt)
+        patr (DefaultHighlighter$DefaultHighlightPainter. Color/LIGHT_GRAY)
+        s    (.toLowerCase (hl/remove-cr (.getText txt)))
+        ptrn (JOptionPane/showInputDialog tabs "Enter search string:" "Find" JOptionPane/QUESTION_MESSAGE)
+        lims (when ptrn (hl/limits (.toLowerCase ptrn) s))]
+    (.removeAllHighlights hl)
+    (doseq [[a b] lims]
+      (.addHighlight hl a b patr))))
+;;-------------------------------
 (defn update-line-numbers [doc lines]
   (let [pos (.getLength doc)
         root (.getDefaultRootElement doc)
@@ -123,21 +140,31 @@
     (doto lines
       (.setText (apply str (interpose "\n" (range 1 (+ n 2)))))
       (.updateUI))))
-
-(defn insert-text [^JTextPane txt s]
-  (let [doc (.getDocument txt)
-        pos (.getCaretPosition txt)]
-    (.insertString doc pos s nil)))
-
+;;------------------------------
+(defn insert-text
+  "Inserts the specified text in the document."
+  ([^JTextPane txt s] (insert-text txt s true))
+  ([^JTextPane txt s restore]
+    (let [doc (.getDocument txt)
+          pos (.getCaretPosition txt)]
+      (.insertString doc pos s nil)
+      (when restore (.setCaretPosition txt pos)))))
+;;------------------------------()(898
 (defn input-format [^JTextPane txt e]
-  (let [k (.getKeyCode e)]
-    (cond (= KeyEvent/VK_LEFT_PARENTHESIS k)
+  "Insert a closing parentesis."
+  (let [c (.getKeyChar e)
+        k (.getKeyCode e)]
+    (cond (= \( c)
             (queue-ui-action #(insert-text txt ")"))
+          (= \{ c)
+            (queue-ui-action #(insert-text txt "}"))
+          (= \[ c)
+            (queue-ui-action #(insert-text txt "]"))
           (= KeyEvent/VK_TAB k)
             (do 
               (.consume e)
-              (queue-ui-action #(insert-text txt "  "))))))
-
+              (queue-ui-action #(insert-text txt "  " false))))))
+;;------------------------------
 (defn new-document
   "Adds a new tab to tabs and sets its title."
   ([tabs] (new-document tabs new-doc-title))
@@ -196,8 +223,9 @@
         (.setBackground Color/black))
 
       txt-code)))
-
+;;------------------------------
 (defn open-src [tabs]
+  "Open source file."
   (let [dialog (JFileChooser. @default-dir)
         result (.showOpenDialog dialog nil)
         file (.getSelectedFile dialog)
@@ -206,42 +234,40 @@
       (reset! default-dir (.getCanonicalPath (File. path)))
       (let [src (slurp path)
             txt-code (new-document tabs path src)]
-            ;doc (.getStyledDocument txt-code)
         (doto txt-code
           (.setCaretPosition 0)
           (.grabFocus))
         (hl/high-light txt-code)))))
-
+;;------------------------------
 (defn close [tabs]
+  "Close the application."
   (let [idx (.getSelectedIndex tabs)]
     (.removeTabAt tabs idx)))
-
-(defn redirect-out [txt]
-  (let [stream (proxy [OutputStream] []
-                 (write
-                   ([b off len] (.append txt (String. b off len)))
-                   ([b] (.append txt (String. b)))))
-        out (PrintStream. stream true)]
-    (System/setOut out)
-    (System/setErr out)))
-
-(def menu-options 
+;;------------------------------
+(def menu-options
   [{:name "File" 
      :items [
         {:name "New" :action #(print "New") :keys [KeyEvent/VK_N KeyEvent/CTRL_MASK]}
       ]
   }]
 )
-
-(defn build-menu [tabs]
+;;------------------------------
+(defn build-menu
+  "Builds the application's menu.
+  TODO: load from data."
+  [tabs txt-repl]
   (let [menubar (JMenuBar.)
-        menu (JMenu. "File")
+        menu-file (JMenu. "File")
         item-new (JMenuItem. "New")
         item-open (JMenuItem. "Open")
         item-save (JMenuItem. "Save")
         item-close (JMenuItem. "Close")
+        item-exit (JMenuItem. "Exit")
+        menu-code (JMenu. "Code")
         item-eval (JMenuItem. "Eval")
-        item-exit (JMenuItem. "Exit")]
+        item-find (JMenuItem. "Find")
+        item-clear (JMenuItem. "Clear Log")]
+
     (on-click item-new #(new-document tabs))
     (.setAccelerator item-new (KeyStroke/getKeyStroke KeyEvent/VK_N KeyEvent/CTRL_MASK))
 
@@ -254,30 +280,74 @@
     (on-click item-close #(close tabs))
     (.setAccelerator item-close (KeyStroke/getKeyStroke KeyEvent/VK_W KeyEvent/CTRL_MASK))
 
+    (on-click item-exit #(System/exit 0))
+    (.setAccelerator item-exit (KeyStroke/getKeyStroke KeyEvent/VK_X KeyEvent/ALT_MASK))
+
+    (.add menu-file item-new)
+    (.add menu-file item-open)
+    (.add menu-file item-save)
+    (.add menu-file item-close)
+    (.add menu-file item-exit)
+
     (on-click item-eval #(eval-src tabs))
     (.setAccelerator item-eval (KeyStroke/getKeyStroke KeyEvent/VK_E KeyEvent/CTRL_MASK))
 
-    (on-click item-exit #(System/exit 0))
-    (.setAccelerator item-exit (KeyStroke/getKeyStroke KeyEvent/VK_X KeyEvent/CTRL_MASK))
+    (on-click item-find #(find-src tabs))
+    (.setAccelerator item-find (KeyStroke/getKeyStroke KeyEvent/VK_F KeyEvent/CTRL_MASK))
 
-    (.add menu item-new)
-    (.add menu item-open)
-    (.add menu item-save)
-    (.add menu item-close)
-    (.add menu item-eval)
-    (.add menu item-exit)
+    (on-click item-clear #(.setText txt-repl ""))
+    (.setAccelerator item-clear (KeyStroke/getKeyStroke KeyEvent/VK_L KeyEvent/CTRL_MASK))
 
-    (.add menubar menu)
+    (.add menu-code item-eval)
+    (.add menu-code item-find)
+    (.add menu-code item-clear)
+
+    (.add menubar menu-file)
+    (.add menubar menu-code)
     menubar))
-
-(defn make-main [name txt-repl]
+;;------------------------------
+(defn redirect-out 
+  "Creates a PrintStream that writes to the
+  JTextArea instance provided and then replaces
+  System/out with this stream."
+  [^JTextArea txt]
+  (let [stream (proxy [OutputStream] []
+                 (write
+                   ([b off len] (.append txt (String. b off len)))
+                   ([b] (.append txt (String. b)))))
+        out (PrintStream. stream true)]
+    (System/setOut out)
+    (System/setErr out)))
+;------------------------------------------
+;------------------------------------------
+(in-ns 'clojure.core)
+(use 'clojure.repl)
+;------------------------------------------
+(defn rebind-out "Allows standard *out* rebinding."
+  [out]
+  (def ^:dynamic *out-original* *out*)
+  (def ^:dynamic *out* out))
+;------------------------------------------
+;------------------------------------------
+(in-ns 'macho.core)
+;------------------------------------------
+(defn make-main 
+  "Creates the main window and all
+  its controls."
+  [name]
   (let [main (JFrame. name)
+        txt-repl (JTextArea.)
         tabs (JTabbedPane.)
         txt-in (JTextArea.)
         pane-repl (JSplitPane.)
         pane-center-left (JSplitPane.)
         pane-all (JSplitPane.)
         files-tree (JTree. (to-array []))]
+
+    ; Redirect std out
+    (redirect-out txt-repl)
+    (clojure.core/rebind-out (java.io.OutputStreamWriter. System/out))
+
     ; Set controls properties
     (doto txt-repl
       (.setEditable false)
@@ -307,22 +377,12 @@
     (doto main
       (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
       (.setSize 800 600)
-      (.setJMenuBar (build-menu tabs))
+      (.setJMenuBar (build-menu tabs txt-repl))
       (.add pane-center-left BorderLayout/CENTER)
       (.setVisible true))))
-;------------------------------------------
-(in-ns 'clojure.core)
-(use 'clojure.repl)
-
-(defn rebind-out "Allows standard *out* rebinding."
-  [out]
-  (def ^:dynamic *out-original* *out*)
-  (def ^:dynamic *out* out))
-;------------------------------------------
-(in-ns 'macho.core)
-
-(defn -main []
-  (def txt-repl (JTextArea.))
-  (redirect-out txt-repl)
-  (clojure.core/rebind-out (java.io.OutputStreamWriter. System/out))
-  (make-main app-name txt-repl))
+;;------------------------------
+(defn -main
+  "Program startup function"
+  []
+  (make-main app-name))
+;;------------------------------
