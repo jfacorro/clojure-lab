4b086dcc3d54a31dceaae202495c1eec71f31e91
Juan Facorro <juan.facorro@gmail.com>
2012-12-11 13:13:53 -0300

    Some tests with the repl process management.

--- src/repl-process.clj
+++ src/repl-process.clj
@@ -1,10 +1,10 @@
 (ns repl-proc
-  (:import 
-        [javax.swing BorderLayout JFrame JTextArea JScrollPane])
+  (:import [javax.swing JFrame JTextArea JScrollPane]
+           [java.awt BorderLayout])
   (:use [popen]
         [clojure.repl]))
 
-(def repl-cmd ["java" "-cp" "../libs/clojure-1.4.0.jar;." "clojure.main"])
+(def repl-cmd ["java" "-cp" "./lib/clojure-1.4.0.jar;./src" "clojure.main"])
 
 (def p (popen repl-cmd :redirect true))
 
@@ -12,11 +12,11 @@
 (def in (stdin p))
 
 (def frame (JFrame.))
-(def txt (doto (JTextArea.) (.setEditable false))
+(def txt (doto (JTextArea.) (.setEditable false)))
 
 (doto frame
   (.setVisible true)
-  (.setSize 400 400))
+  (.setSize 400 400)
   (.add (JScrollPane. txt) BorderLayout/CENTER))
 
 (defn read-out [out txt]
@@ -29,11 +29,15 @@
   (.append txt (str s "\n"))
   (.flush in))
 
+(def exec (partial write-in in))
+
 (def trh-out (Thread. #(read-out out txt)))
 (.start trh-out)
 
-(write-in in "*out*")
-(write-in in "(def repl :repl)")
-(write-in in "repl")
-;(write-in in "(iterate identity 1)")
-(write-in in "(ns macho)")
+;(write-in in "*out*")
+;(write-in in "(def repl :repl)")
+;(write-in in "repl")
+(write-in in "(take 5 (iterate identity 1))")
+;(write-in in "(ns macho)")
+
+
