f5086649d974a805fb952de65e196a3a17a61b6b
Juan Facorro <juan.facorro@gmail.com>
2013-04-18 13:32:54 -0300

    Updated version of clojure dependency. Modified project's version to 0.0.1 until there's a working non-prototype version.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject macho "1.0.0-SNAPSHOT"
+(defproject macho "0.0.1-SNAPSHOT"
   :description "macho (minimal advanced clojure hacking optimizer)"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [net.cgrand/parsley "0.9.1"]
                  [popen "0.2.0"]
                  [leiningen "2.0.0"]
