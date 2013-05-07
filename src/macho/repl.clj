c38719208e8f07f0d207535a24cf0f170f8fb3a1
Juan Facorro <juan.facorro@gmail.com>
2013-05-07 17:29:06 -0300

    Set project.clj directory when opening a REPL for it.

--- src/macho/repl.clj
+++ src/macho/repl.clj
@@ -22,12 +22,14 @@
   command string to launch a child process and then open it."
   ([project-path]
     (let [project (project/init-project (project/read project-path))
+          file    (io/file project-path)
+          dir     (if (.isDirectory file) file (.getParent file))
           init    '((require 'clojure.main))
           main    (if-let [main (:main project)]
                     `((require '~main) (clojure.main/repl :init #(do (in-ns '~main) (require 'clojure.repl))))
                     `((clojure.main/repl)))
           cmd     (eval/shell-command project (concat '(do) init main))
-          proc    (popen/popen cmd :redirect true)]
+          proc    (popen/popen cmd :redirect true :dir dir)]
       (Repl. proc (popen/stdin proc) (popen/stdout proc))))
   ([]
     (let [proc (popen/popen clojure-repl-cmd :redirect true)]
