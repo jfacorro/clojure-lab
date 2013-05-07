6754dad189b2b185d70f1c029c289437ec1b3f9e
Juan Facorro <juan.facorro@gmail.com>
2013-05-07 00:37:44 -0300

    Created records for insert and delete document operations.

--- src/lab/model/document.clj
+++ src/lab/model/document.clj
@@ -2,17 +2,40 @@
   (:refer-clojure :exclude [replace name])
   (:require [lab.model.buffer :as b]
             [lab.util :as util]
+            [lab.model.history :as h]
             [clojure.java.io :as io]))
 
-(defrecord Document [name path modified buffer alternates])
+(declare modified? delete insert)
 
-(declare modified?)
+;; Document operations
+
+(defrecord InsertText [offset s])
+(defrecord RemoveText [start end s])
+
+(extend-protocol hist/Bijective
+  InsertText
+  (inverse [this]
+    (let [offset (:offset this)
+          n      (-> this :s count)]
+      #(delete % offset (+ offset n))))
+
+  RemoveText
+  (inverse [this]
+    (let [start (:start this)
+          s     (:s this)]
+      #(insert % start s))))
+
+;; Document
+
+(defrecord Document [name path modified buffer alternates])
 
 (defn- default-buffer
   "Returns a buffer implementation."
   [& xs]
   (apply b/incremental-buffer xs))
-;-------IO operations--------
+
+;; IO operations
+
 (defn bind
   "Binds a document to a file."
   [doc path]
@@ -33,7 +56,9 @@
   "Saves the documento to a file, if bounded."
   [doc]
   (throw (UnsupportedOperationException. "save doc")))
-;-------Properties--------
+
+;; Properties
+
 (defn name
   "Returns the document's name."
   [doc]
@@ -72,7 +97,9 @@
   match."
   [doc s]
   (util/find-limits s (text doc)))
-;-------Text operations--------
+
+;; Text operations
+
 (defn insert
   "Inserts s at the document's offset position.
   Returns the document."
@@ -100,16 +127,25 @@
         f      (fn [x [s e]]
                  (-> x (delete s e) (insert s rpl)))]
     (reduce f doc limits)))
-;----Document creation function----
+
+;; Document creation function
+
 (defn document
   "Creates a new document using the name and alternate models provided."
   [name & {:keys [path alternates] :or {path nil alternates []}}]
   {:pre [(not (nil? name))]}
-  (let [doc (Document. name nil false (default-buffer) alternates)]
+  (let [doc (map->Document {:name name
+                            :path nil
+                            :modified false
+                            :buffer (default-buffer)
+                            :history (hist/history)
+                            :alternates alternates})]
     (if path
       (bind doc path)
       doc)))
-;----Alternates----
+      
+;; Alternates
+
 (defn add-alternate
   "Adds an alternate model to the map."
   [m k alt]
