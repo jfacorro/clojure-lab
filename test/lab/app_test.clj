19ac7ea0e9f3b9a9e592755f5f25c52700f3fea7
Juan Facorro <juan.facorro@gmail.com>
2013-06-07 01:26:43 -0300

    Added comments, removed unnecesary var in test.

--- test/lab/app_test.clj
+++ test/lab/app_test.clj
@@ -5,21 +5,26 @@
   (:require [clojure.java.io :as io]
             [lab.model.document :as doc]))
 ;;------------------------------------
-(def file-content "Temp file, should be deleted.")
 (def tmp-files ["./tmp" "../tmp"])
 ;;------------------------------------
-(defn delete-file [path]
+(defn delete-file
+  "Deletes a file if it exists."
+  [path]
   (when (-> path io/file .exists)
     (io/delete-file path)))
 ;;------------------------------------
-(defn create-file [path]
-  (spit path file-content))
+(defn create-file
+  "Creates a file with the supplied content."
+  [content path]
+  (spit path content))
 ;;------------------------------------
 (defn temp-document-config
+  "Fixture that creates some temp files and sets
+  the new document counter to 0."
   [f]
   (try
     (binding [doc/*untitled-count* (atom 0)]
-      (dorun (map create-file tmp-files))
+      (dorun (map (partial create-file "Temp file, should be deleted.") tmp-files))
       (f))
     (finally
       (dorun (map delete-file tmp-files)))))
@@ -59,16 +64,15 @@
     (->is = 2 (comp count :documents))
     (->is not= nil (comp :current-document))
     (->is = "tmp" (comp :name deref :current-document))
-    
+
     (open-document "../tmp")
     (->is = 3 (comp count :documents))
     (->is not= nil (comp :current-document))
     (->is = "tmp" (comp :name deref :current-document))
-    
-    (open-document "./tmp")
-    (->is = 3 (comp count :documents))
-    
-    (open-document "../tmp")
+
+    ; Open the same file with different paths
+    ; and check it's still 3 documents.
+    (open-document ".././tmp")
     (->is = 3 (comp count :documents))
     
     (open-document "./../tmp")
