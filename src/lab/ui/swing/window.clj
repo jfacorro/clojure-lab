eaae74d3fdedcd5e886391923fc966eef4fd05a2
Juan Facorro <juan.facorro@gmail.com>
2013-10-06 21:12:21 +0900

    Replaced :use of lab.ui.protocol in lab.ui.swing.window for :require and an alias.

--- src/lab/ui/swing/window.clj
+++ src/lab/ui/swing/window.clj
@@ -1,8 +1,8 @@
 (ns lab.ui.swing.window
   (:import  [javax.swing JFrame])
-  (:use     [lab.ui.protocols :only [Implementation impl]])
   (:require [lab.ui.core :as ui]
-            [lab.ui.swing.util :as util]))
+            [lab.ui.swing.util :as util]
+            [lab.ui.protocols :as p]))
 
 (ui/definitializations :window JFrame)
 
@@ -10,18 +10,20 @@
   :window
     (:maximized [c _ v]
       (when v
-        (.setExtendedState (impl c) (bit-or (.getExtendedState (impl c)) JFrame/MAXIMIZED_BOTH))))
+        (.setExtendedState (p/impl c) 
+                           (bit-or (.getExtendedState (p/impl c)) 
+                                   JFrame/MAXIMIZED_BOTH))))
     (:size [c _ [w h]]
-      (.setSize (impl c) w h))
+      (.setSize (p/impl c) w h))
     (:menu [c _ v]
-      (ui/set-attr c :j-menu-bar (impl v))
-      (.revalidate (impl c)))
+      (ui/set-attr c :j-menu-bar (p/impl v))
+      (.revalidate (p/impl c)))
     (:icons [c _ v]
       (let [icons (map util/image v)]
-        (.setIconImages (impl c) icons))))
+        (.setIconImages (p/impl c) icons))))
 
 (extend-type JFrame
-  Implementation
+  p/Implementation
   (abstract
     ([this] nil)
     ([this the-abstract] this)))
