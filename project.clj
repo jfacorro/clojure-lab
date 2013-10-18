f57b6ec63408908ef6cea574f74ca52f29802f4f
Juan Facorro <juan.facorro@gmail.com>
2013-10-18 20:59:38 +0800

    Added logging libraries and configuration file.

--- project.clj
+++ project.clj
@@ -4,10 +4,16 @@
                  [net.cgrand/parsley "0.9.1"]
                  [popen "0.2.0"]
                  [leiningen-core "2.0.0"]
+                 ; Logging
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.slf4j/slf4j-log4j12 "1.6.6"]
+                 ; Repl console
                  [swingrepl "1.3.0" :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]
+                 ; gtk+
                  [local/gtk "4.1"]
                  [local/gtk-bindings "4.1"]]
-  :manifest {"SplashScreen-Image" "logo.png"}
+  :manifest {"SplashScreen-Image" "logo.png"}  
+  
   :aliases  {"build" ["do" "clean," "uberjar"]
              "build-aot" ["with-profile" "aot" "do" "clean," "uberjar"]}
   :repositories {"local" "file:repo"}
