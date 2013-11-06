725e6302c0e8f9cc318df7899078553107ffb7d7
Juan Facorro <juan.facorro@gmail.com>
2013-11-06 18:01:39 +0800

    Fixed bug in the ordering of tree items.

--- src/lab/ui/tree.clj
+++ src/lab/ui/tree.clj
@@ -3,8 +3,8 @@
 
 (defn file-proxy
   "Creates a proxy that overrides the toString method
-  for the File class so that it returns the (file/directory)'s
-  name."
+for the File class so that it returns the (file/directory)'s
+name."
   [file]
   (proxy [java.io.File] [(.getPath file)]
     (toString []
@@ -17,13 +17,13 @@
 
 (defn- tree-node-from-file
   "Creates a tree node with the supplied file as its item.
-  If the arg is a directory, all its children are added
-  recursively."
+If the arg is a directory, all its children are added
+recursively."
   [file]
   (if (.isDirectory file)
     (let [children (->> file .listFiles 
                      (filter (comp not hidden?))
-                     (sort-by #(if (.isDirectory %) 0 (.getName %)))
+                     (sort-by #(if (.isDirectory %) (str " " (.getName %)) (.getName %)))
                      (map file-proxy))]
       (into [:tree-node {:item file}]
               (->> children (map tree-node-from-file) vec)))
@@ -31,9 +31,8 @@
 
 (defn load-dir
   "Loads the directory tree for the given path. If its a file
-  then the parent directory is considered the root of the 
-  tree.
-  Returns a tree node."
+then the parent directory is considered the root of the 
+tree. Returns a tree node."
   [root-path]
   (let [root  (io/file root-path)
         root  (if (.isDirectory root) root (.getParentFile root))]
