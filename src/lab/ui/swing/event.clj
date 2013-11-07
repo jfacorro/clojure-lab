983874c20bc0924b0390252ad5785ea55bc8fd04
Juan Facorro <juan.facorro@gmail.com>
2013-11-07 18:55:20 +0800

    Modified Event implementation to reuse parents'. Added event hijacking macro for Swing component.

--- src/lab/ui/swing/event.clj
+++ src/lab/ui/swing/event.clj
@@ -8,6 +8,88 @@
                            MouseEvent
                            FocusEvent
                            ActionEvent]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Event hijacking
+
+(def event-types
+  {java.awt.event.MouseEvent      :mouse-event
+   java.awt.event.KeyEvent        :key-event
+   java.awt.event.FocusEvent      :focus-event
+   java.awt.event.WindowEvent     :window-event})
+
+(defn event-type
+  "Goes through the map's keys looking for the class
+to which the event belongs.
+TODO: improve implementation to be able to add parent classes as keys."
+  [e m]
+  (->> m
+    (filter #(instance? (key %) e))
+    first
+    second))
+
+(defmacro hijack-events
+  "Takes a class and a map that defines the event
+types to be hijacked with the handlers to be used.
+Event handlers get an event in the form of a map 
+(through the use of to-map) and a thunk with a call to the
+super's implementation of processEvent(...).
+
+(defn f [x]
+  (fn [e super]
+    (println x \"=>\" e)
+    (super)))
+
+(def handlers
+  {:key        (#'f \"key\")
+   :focus      (#'f \"focus\")})
+
+Returns an instance of the object with the processEvent(...)
+methods overriden. "
+  [clazz handlers]
+  `(proxy [~clazz] []
+    (processEvent [e#]
+      (let [event-type# (event-type e# event-types)
+            handler#    (~handlers event-type#)
+            super#      #(proxy-super processEvent e#)]
+        (if handler#
+          (handler# (p/to-map e#) super#)
+          (super#))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Utility functions
+
+(defn- flag-modifiers [m n]
+  (->> m 
+    (filter #(pos? (bit-and n (second %))))
+    (mapv first)))
+
+(defn- merge-results [f1 f2]
+  #(merge (f1 %) (f2 %)))
+
+(defn- merge-impls [x y]
+  (reduce 
+    (fn [x k] (update-in x [k] merge-results (k y)))
+    x
+    (keys y)))
+
+(defn- build-merged-impl
+  "Receives maps with Event protocol implementations
+and merges the map returned by the to-map function.
+TODO: provide a map with merging functions."
+  [& impls]
+  (reduce merge-impls impls))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Event Object
+
+(def event-object
+  {:to-map (fn [this] {:source  (p/abstract (.getSource this))})})
+
+(extend java.util.EventObject
+  p/Event
+  event-object)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Key Event
 
@@ -20,6 +102,21 @@
   {:alt    InputEvent/ALT_MASK    
    :shift  InputEvent/SHIFT_MASK
    :ctrl   InputEvent/CTRL_MASK})
+
+
+(def input-event
+  {:to-map (fn [this] {:modifiers  (flag-modifiers input-modifiers (.getModifiers this))})})
+
+(def key-event
+  {:to-map (fn [this]
+             {:char         (.getKeyChar this)
+              :code         (.getKeyCode this)
+              :description  (KeyEvent/getKeyText (.getKeyCode this))
+              :event        (key-event-ids (.getID this))})})
+
+(extend KeyEvent
+  p/Event
+  (build-merged-impl event-object input-event key-event))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Mouse Event
@@ -39,6 +136,18 @@
    MouseEvent/MOUSE_RELEASED :released
    MouseEvent/MOUSE_WHEEL    :wheel})
 
+(def mouse-event
+  {:to-map (fn [this]
+             {:button       (mouse-button (.getButton this))
+              :click-count  (.getClickCount this)
+              :screen-loc   (as-> (.getLocationOnScreen this) p [(.getX p) (.getY p)])
+              :point        (as-> (.getPoint this) p [(.getX p) (.getY p)])
+              :event        (mouse-event-ids (.getID this))})})
+
+(extend MouseEvent
+  p/Event
+  (build-merged-impl event-object input-event mouse-event))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Action Event
 
@@ -47,44 +156,23 @@
    :ctrl  ActionEvent/CTRL_MASK
    :shift ActionEvent/SHIFT_MASK})
 
+(def action-event
+  {:to-map (fn [this]
+             {:source    (p/abstract (.getSource this))
+              :modifiers (flag-modifiers action-modifiers (.getModifiers this))})})
+
+(extend ActionEvent
+  p/Event
+  (build-merged-impl event-object action-event))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Utility functions
+;; Focus Event
 
-(defn- flag-modifiers [m n]
-  (->> m 
-    (filter #(pos? (bit-and n (second %))))
-    (mapv first)))
+(def focus-event
+  {:to-map (fn [this]
+             {:previous  (.getOppositeComponent this)
+              :temporary (.isTemporary this)})})
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Event protocol implementation
-
-(extend-protocol p/Event
-  java.util.EventObject
-  (to-map [this]
-    {:source (p/abstract (.getSource this))})
-  KeyEvent
-  (to-map [this]
-    {:source       (p/abstract (.getSource this))
-     :char         (.getKeyChar this)
-     :code         (.getKeyCode this)
-     :description  (KeyEvent/getKeyText (.getKeyCode this))
-     :event        (key-event-ids (.getID this))
-     :modifiers    (flag-modifiers input-modifiers (.getModifiers this))})
-  MouseEvent
-  (to-map [this]
-    {:source       (p/abstract (.getSource this))
-     :button       (mouse-button (.getButton this))
-     :click-count  (.getClickCount this)
-     :screen-loc   (as-> (.getLocationOnScreen this) p [(.getX p) (.getY p)])
-     :point        (as-> (.getPoint this) p [(.getX p) (.getY p)])
-     :event        (mouse-event-ids (.getID this))
-     :modifiers    (flag-modifiers input-modifiers (.getModifiers this))})
-  FocusEvent
-  (to-map [this]
-    {:source    (p/abstract (.getSource this))
-     :previous  (.getOppositeComponent this)
-     :temporary (.isTemporary this)})
-  ActionEvent
-  (to-map [this]
-    {:source    (p/abstract (.getSource this))
-     :modifiers (flag-modifiers action-modifiers (.getModifiers this))}))
+(extend FocusEvent
+  p/Event
+  (build-merged-impl event-object focus-event))
