77e3cb8dfd3f45afbcb4a4f08a535d871311b080
Juan Facorro <juan.facorro@gmail.com>
2013-11-29 13:50:05 +0800

    Moved all code from ns lab.ui.tree to the file-tree plugin's ns.

--- src/lab/plugin/file_tree.clj
+++ src/lab/plugin/file_tree.clj
@@ -1,9 +1,49 @@
 (ns lab.plugin.file-tree
+  "Add an global action that creates a tree control with the dirs and files of
+the specified root dir."
   (:use [lab.core.plugin :only [defplugin]])
   (:require [lab.core.keymap :as km]
             [lab.plugin.main-ui :as main]
-            [lab.ui [core :as ui]
-                    [tree :as tree]]))
+            [lab.ui.core :as ui]
+            [clojure.java.io :as io]))
+
+(defn file-proxy
+  "Creates a proxy that overrides the toString method
+for the File class so that it returns the (file/directory)'s
+name."
+  [file]
+  (proxy [java.io.File] [(.getPath file)]
+    (toString []
+      (.getName this))))
+
+(defn- hidden?
+  "Returns true for files or directories that begin with a dot."
+  [file]
+  (-> file .getName (.startsWith ".")))
+
+(defn- tree-node-from-file
+  "Creates a tree node with the supplied file as its item.
+If the arg is a directory, all its children are added
+recursively."
+  [file]
+  (if (.isDirectory file)
+    (let [children (->> file .listFiles 
+                     (filter (comp not hidden?))
+                     (sort-by #(if (.isDirectory %) (str " " (.getName %)) (.getName %)))
+                     (map file-proxy))]
+      (into [:tree-node {:item file}]
+              (->> children (map tree-node-from-file) vec)))
+    [:tree-node {:item file}]))
+
+(defn load-dir
+  "Loads the directory tree for the given path. If its a file
+then the parent directory is considered the root of the 
+tree. Returns a tree node."
+  [root-path]
+  (let [root  (io/file root-path)
+        root  (if (.isDirectory root) root (.getParentFile root))]
+    (tree-node-from-file root)))
+
 
 (defn- open-document-tree
   "Handler for the click event of an item in the tree."
@@ -17,7 +57,7 @@
   [:tab {:title (.getName file) :border :none}
         [:scroll [:tree {:id      "file-tree"
                          :on-click (partial #'open-document-tree app)
-                         :root     (tree/load-dir file)}]]])
+                         :root     (load-dir file)}]]])
 
 (defn- open-project
   [app _]
