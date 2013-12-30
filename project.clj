ade786c566558f24c1ade6332075ba540b6b1506
Juan Facorro <juan.facorro@gmail.com>
2013-12-30 17:09:12 -0300

    Updated dependencies to the latest version.

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "Clojure Lab - A development environment for Clojure in Clojure."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [net.cgrand/parsley "0.9.1"]
-                 [popen "0.2.0"]
-                 [leiningen-core "2.0.0"]
+                 [net.cgrand/parsley "0.9.2"]
+                 [popen "0.3.0"]
+                 [leiningen-core "2.3.4"]
                  ; Logging
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.slf4j/slf4j-log4j12 "1.6.6"]
+                 [org.slf4j/slf4j-log4j12 "1.7.5"]
                  ; Repl console
                  [swingrepl "1.3.0" :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]
                  ; gtk+
