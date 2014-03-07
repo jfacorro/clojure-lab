99c873db06df01bdf5621480b879a5c0114fb911
Juan Facorro <juan.facorro@gmail.com>
2014-03-07 16:13:12 -0300

    Updated tests.

--- test/lab/test/ui.clj
+++ test/lab/test/ui.clj
@@ -15,7 +15,7 @@
 (deftest find-in-ui
   (testing "Find by id"
     (are [x y] (= x (-> ui-test (find y) (attr :id)))
-      "0"   :#0  
+      "0"   :#0
       "2"   :#2
       "1"   :#1
       nil  :#9)))
@@ -23,4 +23,4 @@
 (deftest update-ui
   (testing "Udpate by id"
     (is (= (assoc-in ui-test [:content 0 :content 1 :attrs :bla] 1)
-           (update ui-test :#3 assoc-in [:attrs :bla] 1)))))
+           (update ui-test :#3 attr :bla 1)))))
